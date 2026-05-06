f79c9c9c69eb24d416ae9d7903cd6b2ba696336d
Mohammed ahadullah <ahad53344@gmail.com>
2026-05-06 23:26:42 +0530

    update project structure and add blog section

--- hctpl-recruitpro/src/app/test/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage() {
-  return (
-    <div style={{ padding: '100px', textAlign: 'center', fontFamily: 'sans-serif' }}>
-      <h1 style={{ fontSize: '48px', color: '#1e59f5' }}>RecruitPro Standalone</h1>
-      <p style={{ fontSize: '20px', color: '#666' }}>If you can see this, the server is working correctly.</p>
-    </div>
-  );
-}
